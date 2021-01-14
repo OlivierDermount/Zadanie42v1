4ef9c1e634e3c18b7194ccbdcfff2656712e1722
OlivierDermount <77295229+OlivierDermount@users.noreply.github.com>
2021-01-14 19:25:52 +0100

    Add files via upload
    Dodano ręcznie

---
+++ Zadanie42/FisBas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Zadanie42
+{
+    class FisBas
+    {
+        public string WriteScrReadKeys(string text) //.......
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
+        }
+        public string Fis(string liczbaString)
+        {
+            string fIs;
+            var liczbaInt = int.Parse(liczbaString);
+            if ((liczbaInt % 3) == 0) fIs = "Fis";
+            else fIs = "";
+            return fIs;
+        }
+        public string Bas(string liczbaString)
+        {
+            string bAs;
+            var liczbaInt = int.Parse(liczbaString);
+            if ((liczbaInt % 5) == 0) bAs ="Bas";
+            else bAs = "";
+            return bAs;
+        }
+    }
+}
---
+++ Zadanie42/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fisBas = new FisBas();
+           
+            while (true)
+            {
+                try                                                             //.....
+                {
+                    var liczbaString = fisBas.WriteScrReadKeys("Podaj liczbę");                    
+                    var fIs = fisBas.Fis(liczbaString);
+                    var bAs = fisBas.Bas(liczbaString);
+                    var lString = (fIs == "Fis" || bAs == "Bas") ?
+                                    liczbaString = "" : liczbaString;
+                    var gramyDalej = fisBas.WriteScrReadKeys($"\n " +
+                                         $"{ fIs }{bAs}{lString} " +
+                                                  $"\n \n Gramy dalej ? t / n");
+                    Console.Clear();
+                    if (gramyDalej != "t") break;
+                }
+                catch (Exception ex) // ....
+                {
+                    fisBas.WriteScrReadKeys(ex.Message+"\n \n Wciśnij Enter");
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
